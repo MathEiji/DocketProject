439733e0d765934bba96450506732ea4be0d3996
Matheus Eiji - GTI <matheus.eiji@gmail.com>
2019-11-18 15:09:26 -0300

    annotations

--- src/main/java/com/docket/project/model/Cartorio.java
+++ src/main/java/com/docket/project/model/Cartorio.java
@@ -1,16 +1,19 @@
 package com.docket.project.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 
 @Entity
+@Table(name = "CARTORIO")
 public class Cartorio {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(generator = "increment")
+    @GenericGenerator(name = "increment", strategy = "increment")
     private Integer id;
+
+    @Column(name = "NOME")
     private String nome;
 
     public Cartorio() { }
